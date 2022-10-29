2ca6d158ae99472a73ed8f274df0dcf77b60d906
davidjr82 <davidjr82@users.noreply.github.com>
2022-10-29 06:25:33 +0000

    deploy: 62fc2a13018c0785863dc7e9efb969c440bfed0a

--- assets/examples_basic.md.b0e0bff5.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o,a as n}from"./app.43137063.js";const g=JSON.parse('{"title":"Basic example","description":"","frontmatter":{},"headers":[],"relativePath":"examples/basic.md"}'),l={name:"examples/basic.md"},t=n("",7),p=[t];function e(r,c,D,F,y,i){return o(),a("div",null,p)}const b=s(l,[["render",e]]);export{g as __pageData,b as default};
